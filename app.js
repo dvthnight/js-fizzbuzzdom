1de553ce6faa0f4715ba1c0b48e8700673176c60
dvthnight <davide.lanotte@protonmail.com>
2022-02-07 17:46:15 +0100

    completato milestone 1

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("ciao")
+// creare un ciclo for che mi generi i 100 numeri
+
+for(let i = 1; i<=100; i++){
+    const li = document.createElement("li");
+    
+    console.log(i);
+
+    if(i % 3 === 0 && i % 5 === 0){
+        li.append("fizzbuzz");
+    } else if(i % 3 ===0){
+        li.append("fizz");
+    } else if(i % 5 === 0){
+        li.append("buzz");
+    } else {
+        li.append(i);
+    }
+
+    console.log(li);
+
+    const lista = document.getElementById("lista");
+
+    lista.append(li);
+
+}
